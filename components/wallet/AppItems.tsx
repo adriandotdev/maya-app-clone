09423758b3f4030e97e722e7c253b35406c9e636
Adrian Nads Marcelo <marcelonads@gmail.com>
2026-02-22 22:17:20 +0800

    adjust app item list in wallet page

--- components/wallet/AppItems.tsx
+++ components/wallet/AppItems.tsx
@@ -6,20 +6,18 @@ export default function AppItems() {
 		<>
 			<View
 				style={{
-					marginTop: 16,
-					display: "flex",
 					flexDirection: "row",
-					gap: 8,
+					flexWrap: "wrap",
 				}}
 			>
-				{appListItems.slice(0, 4).map((item, index) => (
+				{appListItems.map((item, index) => (
 					<View
 						key={index}
 						style={{
-							display: "flex",
+							flexBasis: "25%",
 							alignItems: "center",
+							padding: 6,
 							gap: 8,
-							flex: 1,
 						}}
 					>
 						<View
@@ -43,53 +41,6 @@ export default function AppItems() {
 								fontFamily: "Poppins_600SemiBold",
 								color: "#717171",
 								lineHeight: 16, // keeps consistent spacing
-							}}
-						>
-							{item.label}
-						</Text>
-					</View>
-				))}
-			</View>
-
-			<View
-				style={{
-					marginTop: 16,
-					display: "flex",
-					flexDirection: "row",
-					gap: 8,
-				}}
-			>
-				{appListItems.slice(4).map((item, index) => (
-					<View
-						key={index}
-						style={{
-							display: "flex",
-							alignItems: "center",
-							gap: 8,
-							flex: 1,
-						}}
-					>
-						<View
-							style={{
-								backgroundColor: "#f4f6f5",
-								height: 60,
-								width: "100%",
-								display: "flex",
-								justifyContent: "center",
-								alignItems: "center",
-								borderRadius: 10,
-							}}
-						>
-							<item.icon color={"#00a652"} size={18} />
-						</View>
-						<Text
-							style={{
-								maxWidth: 80,
-								textAlign: "center",
-								fontSize: 12,
-								fontFamily: "Poppins_600SemiBold",
-								color: "#717171",
-								lineHeight: 16,
 							}}
 						>
 							{item.label}
